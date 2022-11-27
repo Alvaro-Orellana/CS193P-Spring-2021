72286cb4cfbd41ca8895eb1cf629e8c264f8704f
Alvaro <alvher.orellana@gmail.com>
2022-11-26 22:01:58 -0300

    started new project for assignment 3

---
+++ Assignment 2/Assignment III: Set/Assignment_III__SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_III__SetApp.swift
+//  Assignment III: Set
+//
+//  Created by Alvaro Orellana on 26-11-22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_III__SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment 3/Set/Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Alvaro Orellana on 26-11-22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
